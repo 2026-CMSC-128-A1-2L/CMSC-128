ed3ed281b0becbe630bcbbaa5fcca8b78c582281
Haraya Amihan S. Ching <harayaching@gmail.com>
2026-03-03 00:33:15 +0800

    Feat: report schema added

--- backend/src/models/admin/Report.js
+++ backend/src/models/admin/Report.js
@@ -1,12 +1,43 @@
 import mongoose from 'mongoose';
 
-//Creates an object model for Sample
-const sampleSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  type: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: String,
-  quantity: { type: Number, required: true },
-});
 
-export const Sample = mongoose.model('Sample', sampleSchema);
+//Schema Listing report
+const listingReportSchema = new mongoose.Schema({
+    // Assumed that Mongo will create Primary Key
+
+    reportType: { type: String, required: true },
+
+    ReportFlags: [String],                                  // Array of valid flags
+
+    reportDescription: { type: String, required: true },
+    reportEvidence: { type: String, required: true },       // Can be a link/picture?
+
+    dateCreated: { type: Date, required: true },
+    dateResolved: { type: Date, required: true },
+    reporterID: {type: mongoose.Schema.Types.ObjectId, ref:'Users' , required: true},                       // Foreign key to reporting user
+    listingID: {type: mongoose.Schema.Types.ObjectId, ref:'Listings' , required: true},                     // Forign key for housing reported (to change)
+
+})
+
+//Schema User report
+const userReportSchema = new mongoose.Schema({
+    // Assumed that Mongo will create Primary Key
+
+    reportType: { type: String, required: true },
+
+    ReportFlags: [String],                                  // Array of valid flags
+
+    reportDescription: { type: String, required: true },
+    reportEvidence: { type: String, required: true },       // Can be a link/picture?
+
+    dateCreated: { type: Date, required: true },
+    dateResolved: { type: Date, required: true },
+    reporterID: {type: mongoose.Schema.Types.ObjectId, ref:'Users' , required: true},                   // Foreign key to reporting user
+    userID: {type: mongoose.Schema.Types.ObjectId, ref:'Users' , required: true},                       // Foreign key for reported user
+
+
+})
+
+
+export const ListingReport = model('ListingReport', listingReportSchema);             
+export const UserReport = model('UserReport', userReportSchema);
