1b62785c4bca56348f7d7d705e1e253d4d71436a
HarayaChing <harayaching@gmail.com>
2026-03-03 02:12:27 +0000

    Feat: message schema

--- backend/src/models/communication/Message.js
+++ backend/src/models/communication/Message.js
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
-//Creates an object model for Sample
-const sampleSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  type: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: String,
-  quantity: { type: Number, required: true },
-});
 
-export const Sample = mongoose.model('Sample', sampleSchema);
+//Schema message
+const messageSchema = new mongoose.Schema({
+    // Assumed that Mongo will create Primary Key
+
+    message_content: { type: String, required: true },
+    dateCreated: { type: Date, required: true },
+    dateSeen: { type: Date, required: true },
+    senderID: {type: mongoose.Schema.Types.ObjectId, ref:'Users' , required: true},    // Foreign for sender
+
+  })
+
+export const Message = model('Message', messageSchema);             
