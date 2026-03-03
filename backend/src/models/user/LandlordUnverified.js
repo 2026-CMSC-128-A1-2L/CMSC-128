e0142da6104abcf0e9cd04fde6f665ef29b3985d
JiroKristoferTipan <jttipan1@up.edu.ph>
2026-03-03 22:13:15 +0800

    chore: added more info on added and suggested attributes to remove

--- backend/src/models/user/LandlordUnverified.js
+++ backend/src/models/user/LandlordUnverified.js
@@ -2,6 +2,7 @@
 ik looks very bloated sa attributes, applicants and payments seem useless
 since need nila maverify para gamitin app, IDimage namention ko na medj similar sa ProfilePicture
 pero ngayon ko lang napansin, ano pinagkaiba ng UserInformation sa Name, Contact, Email?
+also need pa ba ng apps + payments yung unverified? di naman nila magagamit app until verified sila
 */
 
 import mongoose from 'mongoose';
@@ -9,18 +10,18 @@ import mongoose from 'mongoose';
 //Creates an object model for Sample
 const landlordUnverifiedSchema = new mongoose.Schema({
   LandlordID: { type: mongoose.Schema.Types.ObjectId, unique: true },
-  Name: { type: String, required: true },                       //user attributes
-  ProfilePicture: { type: String, required: true },             //unsure paano istore yung image
+  Name: { type: String, required: true },                                 //user attributes
+  ProfilePicture: { type: String, required: true },                       //unsure paano istore yung image
   Contact: { type: Number, required: true },
   Email: { type: String, required: true },
-  HousingFacility: { type: String, ref: 'HousingFacility' },    //landlord attributes
+  HousingFacility: { type: String, ref: 'HousingFacility' },              //landlord attributes
   ManagerID: { type: mongoose.Schema.Types.ObjectId, ref: 'Manager' },
-  Applicants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ApplicationForm' }],
-  Payments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Payment' }],
-  IDImage: { type: String },                                    //unverified landlord attributes
+  Applicants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ApplicationForm' }],   //to rmeove
+  Payments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Payment' }],             //to remove
+  IDImage: { type: String },                                              //unverified landlord attributes
   DocumentsURL: { type: String },
   UserInformation: { type: String },
-  VerificationApplicationStatus: { type: Number, default: 0 }   //0 = Pending, 1 = Approved, 2 = Rejected
+  VerificationApplicationStatus: { type: Number, default: 0 }             //0 = Pending, 1 = Approved, 2 = Rejected
   //need pa ng messages
 });
 
