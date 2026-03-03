5c0f4f97129dd79746fd6464dc47c5a20b00e755
JiroKristoferTipan <jttipan1@up.edu.ph>
2026-03-03 22:13:15 +0800

    chore: added more info on added and suggested attributes to remove

--- backend/src/models/user/Landlord.js
+++ backend/src/models/user/Landlord.js
@@ -3,6 +3,9 @@ basically user attributes + landlord attributes
 and kung anoman connected sa landlord sa erd
 akala ko landlord yung higher in command bat
 manager maghihire ng landlord
+
+inadd ko pala yung HousingFacility and ManagerID pala like macheck natin kung anong
+facility yung kaya nila iedit and kung sino manager nila
 she mongo on my db till i atlas
 */
 
@@ -11,12 +14,12 @@ import mongoose from 'mongoose';
 //Creates an object model for Sample
 const landlordSchema = new mongoose.Schema({
   LandlordID: { type: mongoose.Schema.Types.ObjectId, unique: true },
-  Name: { type: String, required: true },                       //user attributes
-  ProfilePicture: { type: String, required: true },             //unsure paano istore yung image
+  Name: { type: String, required: true },                                 //user attributes
+  ProfilePicture: { type: String, required: true },                       //unsure paano istore yung image
   Contact: { type: Number, required: true },
   Email: { type: String, required: true },
-  HousingFacility: { type: String, ref: 'HousingFacility' },    //landlord attributes
-  ManagerID: { type: mongoose.Schema.Types.ObjectId, ref: 'Manager' },
+  HousingFacility: { type: String, ref: 'HousingFacility' },              //landlord attributes, new attribute
+  ManagerID: { type: mongoose.Schema.Types.ObjectId, ref: 'Manager' },    //new attribute
   Applicants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ApplicationForm' }],
   Payments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Payment' }]
   //need pa ng messages
