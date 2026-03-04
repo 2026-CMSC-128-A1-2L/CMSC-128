e98dcd6ae93f130c493c973ee775f0330d8d8398
Nyeethan <cunanannathaniel04@gmail.com>
2026-03-04 15:27:16 +0000

    fix: edited attributes in TransferRequest

--- backend/src/models/student-actions/TransferRequest.js
+++ backend/src/models/student-actions/TransferRequest.js
@@ -2,10 +2,27 @@ import mongoose from "mongoose";
 
 //Creates an object model for TransferRequest
 const TransferRequestSchema = new mongoose.Schema({
-  StudentEmail: { type: String, required: true },
-  UnitId: { type: mongoose.Schema.Types.ObjectId, ref: "Unit" },
-  description: { type: String },
-  DateAndTime: { type: Date },
+  studentID: {
+    type:     mongoose.Schema.Types.ObjectId,
+    ref:      'VerifiedStudent', // Interaction point: PAYS relationship
+    required: true
+  },
+
+  unitID: {
+    type:     mongoose.Schema.Types.ObjectId,
+    ref:      'Unit',            // Interaction point: payment is for this unit
+    required: true
+  },
+  
+  description: {
+    type:     String,
+    required: false
+  },
+
+  dateAndTime: {
+    type:    Date,
+    default: Date.now
+  }
 });
 
 export const TransferRequest = mongoose.model("TransferRequest", TransferRequestSchema);
