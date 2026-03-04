29730cbd2533c92cb644858f71976093d199ef6e
Valentine-real <vcalamillo@up.edu.ph>
2026-03-04 16:19:09 +0800

    Docs: Rename schema

--- backend/src/models/student-actions/TransferRequest.js
+++ backend/src/models/student-actions/TransferRequest.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
-//Creates an object model for Sample
-const sampleSchema = new mongoose.Schema({
+//Creates an object model for TransferRequest
+const TransferRequestSchema = new mongoose.Schema({
   StudentEmail: { type: String, required: true },
   UnitId: { type: mongoose.Schema.Types.ObjectId, ref: "Unit" },
   description: { type: String },
   DateAndTime: { type: Date },
 });
 
-export const Sample = mongoose.model("Sample", sampleSchema);
+export const TransferRequest = mongoose.model("TransferRequest", TransferRequestSchema);
+
