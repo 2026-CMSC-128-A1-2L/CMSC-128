d35c74aed21ebd3554af7ec6f1fa453f8682adca
Nyeethan <cunanannathaniel04@gmail.com>
2026-03-04 15:49:18 +0000

    fix: edited attributes in LandlordVerified

--- backend/src/models/user/LandlordVerified.js
+++ backend/src/models/user/LandlordVerified.js
@@ -7,16 +7,12 @@ import mongoose from 'mongoose';
 
 //Creates an object model for Sample
 const landlordVerifiedSchema = new mongoose.Schema({
-  LandlordID: { type: mongoose.Schema.Types.ObjectId, unique: true },
-  Name: { type: String, required: true },                       //user attributes
-  ProfilePicture: { type: String, required: true },             //unsure paano istore yung image
-  Contact: { type: Number, required: true },
-  Email: { type: String, required: true },
-  HousingFacility: { type: String, ref: 'HousingFacility' },    //landlord attributes
-  ManagerID: { type: mongoose.Schema.Types.ObjectId, ref: 'Manager' },
-  Applicants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ApplicationForm' }],
-  Payments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Payment' }]
-  //need pa ng messages
+  landlordVerifiedID: { type: mongoose.Schema.Types.ObjectId, unique: true },
+  userID: {
+    type:     mongoose.Schema.Types.ObjectId,
+    ref:      'User',   // Interaction point: IS-A User
+    required: true
+  },
 });
 
 export const LandlordVerified = mongoose.model('LandlordVerified', landlordVerifiedSchema);
