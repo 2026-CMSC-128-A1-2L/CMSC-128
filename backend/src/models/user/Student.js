d83e4d220e00901f9b1b53d84a83cb3e7c901266
Nyeethan <cunanannathaniel04@gmail.com>
2026-03-04 14:48:13 +0000

    fix: edited naming in Student schema.

--- backend/src/models/user/Student.js
+++ backend/src/models/user/Student.js
@@ -3,16 +3,12 @@ import mongoose from 'mongoose';
 // Creates an object model for Student
 const studentSchema = new mongoose.Schema({
 
-    user_id: {
+    userID: {
         type:mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
     },
-    student_id: {
-        type:mongoose.Schema.Types.ObjectId,
-        ref: 'StudentVerified',
-        required: true
-    },
+
 }, { timestamps: true });
 
 export const Student = mongoose.model('Student', managerSchema);
