2027236a260ba926df3c37b9c989ad387374c7a3
lvdejesus <lvdejesus1@up.edu.ph>
2026-03-05 15:44:01 +0800

    feat: add error middleware

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { errorHandler } from './routes/error.js';
 import {
   routeGetFacilities,
   routeCreateFacility,
@@ -52,5 +53,7 @@ router.post('/units', routeCreateUnit); // correct manager/landlord, should have
 router.get('/units/:unit_id', routeGetUnitById); // correct manager/landlord (and user?)
 router.patch('/units/:unit_id', routeUpdateUnit); // correct manager/landlord
 router.delete('/units/:unit_id', routeDeleteUnit); // correct manager/landlord
+
+router.use(errorHandler);
 
 export { router as apiRouter };
