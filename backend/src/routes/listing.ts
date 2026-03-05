68b47f12572f31a856fa7dcc0ce550ce88f4c9f8
lvdejesus <lvdejesus1@up.edu.ph>
2026-03-05 15:35:11 +0800

    feat: add initial route handlers

---
+++ backend/src/routes/listing.ts
@@ -0,0 +1,10 @@
+import { RequestHandler } from 'express';
+
+export const routeGetListings: RequestHandler = async (req, res, next) => {};
+export const routeCreateListing: RequestHandler = async (req, res, next) => {};
+
+export const routeGetListingById: RequestHandler = async (req, res, next) => {};
+export const routeUpdateListing: RequestHandler = async (req, res, next) => {};
+export const routeDeleteListing: RequestHandler = async (req, res, next) => {};
+
+export const routeGetUnitsByListing: RequestHandler = async (req, res, next) => {};
