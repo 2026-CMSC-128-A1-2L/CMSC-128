daa8b72d9ab25a7bac6e12b030231bd7cfa986a3
dlocsin13 <dalocsin@up.edu.ph>
2026-03-07 20:02:17 +0800

    feat: added getFacilityById and updateFacility services

--- backend/src/services/facility.ts
+++ backend/src/services/facility.ts
@@ -16,6 +16,18 @@ export type CreateFacilityArguments = {
   documentUrls?: string;
 };
 
+// NOTE: attributes to update are not yet finalized
+export type UpdateFacilityArguments = {
+  managerID?: mongoose.Types.ObjectId;
+  name?: string;
+  type?: string;
+  location?: string;
+  applicationCloseDate?: Date;
+  applicationOpenDate?: Date;
+  documentUrls?: string;
+};
+
+
 export const createFacility = async (data: CreateFacilityArguments) => {
   if (data.applicationCloseDate < data.applicationOpenDate) {
     throw new AppError(422, 'Application close date should not be before application open date.');
@@ -39,4 +51,28 @@ export const createFacility = async (data: CreateFacilityArguments) => {
   });
 
   return await newFacility.save();
+};
+
+
+export const getFacilityById = async(facilityID: mongoose.Types.ObjectId) => {
+  const facility = await HousingFacility.findById(facilityID);
+
+  if(!facility){
+    throw new AppError(404, 'Facility not found.');
+  }
+
+  return facility;
+};
+
+export const updateFacility = async(facility: mongoose.Document & any, data: UpdateFacilityArguments) => {
+  const applicationOpenDate = data.applicationOpenDate ?? facility.applicationOpenDate;
+  const applicationCloseDate = data.applicationCloseDate ?? facility.applicationCloseDate;
+
+  if(applicationOpenDate && applicationCloseDate && applicationCloseDate < applicationOpenDate){
+    throw new AppError(422, 'Application close date should not be before application open date.');
+  }
+
+  Object.assign(facility, data); 
+
+  return await facility.save();
 };
