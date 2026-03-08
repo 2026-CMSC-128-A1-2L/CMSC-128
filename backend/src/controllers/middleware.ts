5bd5d09648d7aa58cc4254b5853f0c45544deb2b
lvdejesus <lvdejesus1@up.edu.ph>
2026-03-08 13:39:59 +0800

    fix: incorrect filters

--- backend/src/controllers/middleware.ts
+++ backend/src/controllers/middleware.ts
@@ -25,7 +25,7 @@ export const correctManagerOrLandlordFilter: RequestHandler = async (req, res, n
   }
 
   res.locals.filters = combineFilters(res.locals.filters, {
-    $or: [{ manager: req.user!._id }, { landlord: req.user!._id }],
+    $or: [{ managerID: req.user!._id }, { landlordID: req.user!._id }],
   });
 
   next();
@@ -36,7 +36,7 @@ export const correctLandlordFilter: RequestHandler = async (req, res, next) => {
     return next(new AppError(401, 'Unauthenticated'));
   }
 
-  res.locals.filters = combineFilters(res.locals.filters, { landlord: req.user!._id });
+  res.locals.filters = combineFilters(res.locals.filters, { landlordID: req.user!._id });
 
   next();
 };
