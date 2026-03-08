c3a164f85a2db51d1453fcf36add6148f9b2dca9
lvdejesus <lvdejesus1@up.edu.ph>
2026-03-08 14:03:30 +0800

    fix: add missing parameter type in buildListingQuery from incorrect merge

--- backend/src/services/listing.ts
+++ backend/src/services/listing.ts
@@ -65,8 +65,7 @@ export const createListing = async (data: CreateListingArguments, filters: any) 
   return await newListing.save();
 };
 
-export function buildListingQuery(args: 
-                                  ): QueryFilter<typeof Listing> {
+export function buildListingQuery(args: GetListingArguments): QueryFilter<typeof Listing> {
   const query: QueryFilter<typeof Listing> = {
     isPrivate: args.isPrivate,
     allowVisit: args.allowVisit,
