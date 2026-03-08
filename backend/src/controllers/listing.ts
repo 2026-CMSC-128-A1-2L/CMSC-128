12560c9f4ef4c9e399cb5a87f40c764191e0518a
Haraya Amihan S. Ching <harayaching@gmail.com>
2026-03-08 15:29:26 +0800

    chore:fixed spelling error

--- backend/src/controllers/listing.ts
+++ backend/src/controllers/listing.ts
@@ -96,5 +96,4 @@ export const routeGetListingById: RequestHandler = async (req, res, next) => {
 export const routeGetListingReviewsById: RequestHandler = async (req, res, next) => {};
 export const routeUpdateListing: RequestHandler = async (req, res, next) => {};
 export const routeDeleteListing: RequestHandler = async (req, res, next) => {};
-
 export const routeGetUnitsByListing: RequestHandler = async (req, res, next) => {};
