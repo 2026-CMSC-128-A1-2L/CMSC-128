d42c383f1a52801f05c7136a1e28d420a8e8ed23
Valentine-real <vcalamillo@up.edu.ph>
2026-03-10 03:39:23 +0800

    feat: implemented getListingReviewsById and updating/deleting listings (NOTE: NOT TESTED)

--- backend/src/controllers/listing.ts
+++ backend/src/controllers/listing.ts
@@ -5,6 +5,10 @@ import {
   getListingById,
   GetListingArguments,
   getListings,
+  getListingReviewsById,
+  updateListing,
+  UpdateListingArguments,
+  deleteListing,
 } from '../services/listing.js';
 import z from 'zod';
 import mongoose from 'mongoose';
@@ -118,8 +122,46 @@ export const routeGetListingById: RequestHandler = async (req, res, next) => {
   res.status(200).json(listing); // sends a json of requested
 };
 
-export const routeGetListingReviewsById: RequestHandler = async (req, res, next) => {};
-export const routeUpdateListing: RequestHandler = async (req, res, next) => {};
-export const routeDeleteListing: RequestHandler = async (req, res, next) => {};
+export const routeGetListingReviewsById: RequestHandler = async (req, res, next) => {
+  const listingID = objectIdSchema.parse(req.params.listingId);
+  const reviews = await getListingReviewsById(listingID);
+
+  res.status(200).json({
+    data: reviews
+  });
+};
+
+export const routeUpdateListing: RequestHandler = async (req, res, next) => {
+  const ParamsSchema = z.object({
+    tags: z.array(z.string()).optional(),
+    roomType: z.string().optional(),
+    capacity: z.number().optional(),
+    isPrivate: z.boolean().optional(),
+    allowVisit: z.boolean().optional(),
+    allowTransfer: z.boolean().optional(),
+    description: z.string().optional(),
+    mediaUrls: z.array(z.string()).optional(),
+    units: z.array(z.string()).optional(),
+  });
+
+  const listingID = objectIdSchema.parse(req.params.listingId);
+  const updateData = ParamsSchema.parse(req.body);
+
+  const updatedListing = await updateListing(listingID, updateData, res.locals.filters ?? {});
+
+  res.status(200).json({
+    data: updatedListing,
+  });
+};
+
+export const routeDeleteListing: RequestHandler = async (req, res, next) => {
+  const listingID = objectIdSchema.parse(req.params.listingId);
+  
+  await deleteListing(listingID, res.locals.filters ?? {});
+
+  res.status(200).json({
+    message: 'Listing deleted successfully.',
+  });
+};
 
 export const routeGetUnitsByListing: RequestHandler = async (req, res, next) => {};
