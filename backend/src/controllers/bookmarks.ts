3c75ddd68ea6897f114a0a1921d98c78804689bb
lvdejesus <lvdejesus1@up.edu.ph>
2026-03-18 12:55:30 +0800

    feat: add initial controller and service for bookmarks

--- backend/src/controllers/bookmarks.ts
+++ backend/src/controllers/bookmarks.ts
@@ -1,5 +1,23 @@
 import { RequestHandler } from 'express';
+import { createBookmark, deleteBookmark, getBookmarksByUser } from '../services/bookmarks';
+import { CreateBookmarkParamsSchema } from './schema/bookmark';
 
-export const routeGetBookmarkedUnits: RequestHandler = async (req, res, next) => {};
-export const routeAddBookmark: RequestHandler = async (req, res, next) => {};
-export const routeDeleteBookmark: RequestHandler = async (req, res, next) => {};
+export const routeGetBookmarkedUnits: RequestHandler = async (req, res, next) => {
+  const userID = req.user!._id;
+  const result = await getBookmarksByUser(userID);
+  res.status(200).json(result);
+};
+
+export const routeAddBookmark: RequestHandler = async (req, res, next) => {
+  const params = CreateBookmarkParamsSchema.parse(req.params);
+  const userID = req.user!._id;
+  const result = await createBookmark(userID, params.listingID);
+  res.status(201).json(result);
+};
+
+export const routeDeleteBookmark: RequestHandler = async (req, res, next) => {
+  const params = CreateBookmarkParamsSchema.parse(req.params);
+  const userID = req.user!._id;
+  const result = await deleteBookmark(userID, params.listingID);
+  res.status(200).json(result);
+};
