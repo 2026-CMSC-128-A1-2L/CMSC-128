0b9e0910a05f19ad530d13ac2de4d33712883469
lvdejesus1 <lvdejesus1@up.edu.ph>
2026-03-24 15:18:05 +0800

    feat: add facility and user types to constants

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,1 +1,4 @@
-export const ROOM_TYPES = ['single', 'double', 'shared'];
+export const ROOM_TYPES = ['single', 'double', 'shared'] as const;
+export const FACILITY_TYPES = ['on-campus', 'off-campus', 'partner housing'] as const;
+export const USER_TYPES = ['Admin', 'Landlord', 'Manager', 'Student', 'UnverifiedLandlord', 'UnverifiedManager', 'UnverifiedStudent'] as const;
+
