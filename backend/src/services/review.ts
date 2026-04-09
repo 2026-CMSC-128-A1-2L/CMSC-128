64a4ece23007d9e8ba75733e16633b7d23e3e3c2
TedVillanueva <tcvillanueva2@up.edu.ph>
2026-04-10 03:29:05 +0800

    feat: add createReview, updateReview, deleteReview

--- backend/src/services/review.ts
+++ backend/src/services/review.ts
@@ -5,6 +5,40 @@ import { AppError } from '../controllers/error.js';
 import { combineFilters } from '../controllers/middleware.js';
 import { HousingFacility } from '../models/housing/HousingFacility.js';
 
+export type createReviewArguments = {
+  studentId: mongoose.Types.ObjectId, 
+  listingId: mongoose.Types.ObjectId;
+  rating: number;
+  description?: String;
+}
+
+export type updateReviewArguments = {
+  reviewId: mongoose.Types.ObjectId,
+  rating?: number;
+  description?: String;
+}
+
+export const createReview = async (
+  listingId: mongoose.Types.ObjectId, 
+  data: createReviewArguments,
+  filters: any,
+) => {
+  const listing = Listing.findOne(combineFilters({_id: listingId}, filters));  
+
+  if(!listing){
+    throw new AppError(404, 'Listing not found.');
+  }
+  
+  const newReview = new Review({
+    studentId: data.studentId,
+    listingId: data.listingId,
+    rating: data.rating,
+    description: data.description
+  });
+
+  await newReview.save();
+};
+
 export const getReviews = async (filters: any) => {
   const visibleListings = await Listing.find(filters).select('_id');
 
@@ -60,4 +94,25 @@ export const getFacilityReviews = async (
   return await Review.find({
     listingID: { $in: listingIDs },
   });
+};
+
+export const updateReview = async (data: updateReviewArguments) => {
+  const review = await Review.findById(data.reviewId);
+  if(!review){
+    throw new AppError(404, 'Review not found.');
+  };
+
+  review.set(data);
+
+  return await review.save();
+};
+
+export const deleteReview = async (reviewId: mongoose.Types.ObjectId) => {
+  const review = await Review.findById(reviewId);
+  
+  if(!review){
+    throw new AppError(404, 'Review not found.');
+  }
+
+  return await Review.findByIdAndDelete(reviewId);
 };
