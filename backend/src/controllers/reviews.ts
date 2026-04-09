4b5819e9b915c0ead6d2f944ee709d30452c8099
TedVillanueva <tcvillanueva2@up.edu.ph>
2026-04-10 03:34:18 +0800

    feat: add review CRUD controllers

--- backend/src/controllers/reviews.ts
+++ backend/src/controllers/reviews.ts
@@ -1,6 +1,17 @@
 import { RequestHandler } from 'express';
 import { ObjectIdSchema } from './schema/common.js';
-import { getReviews, getListingReviews, getFacilityReviews } from '../services/review.js';
+import { getReviews, getListingReviews, getFacilityReviews, createReview, deleteReview, updateReview } from '../services/review.js';
+import { CreateReviewBodySchema, UpdateReviewBodySchema } from './schema/review.js';
+
+export const routeCreateReview: RequestHandler = async (req, res, next) => {
+  const listingId = ObjectIdSchema.parse(req.params.listingId);
+
+  const params = CreateReviewBodySchema.parse(req.body);
+
+  const createdReview = await createReview(listingId, params, res.locals.filters ?? {});
+
+  res.status(201).json({ data: createdReview });
+};
 
 export const routeGetReviews: RequestHandler = async (req, res, next) => {
   const reviews = await getReviews(
@@ -11,9 +22,7 @@ export const routeGetReviews: RequestHandler = async (req, res, next) => {
     data: reviews,
   });
 };
-export const routeCreateReview: RequestHandler = async (req, res, next) => { };
-export const routeUpdateReview: RequestHandler = async (req, res, next) => { };
-export const routeDeleteReview: RequestHandler = async (req, res, next) => { };
+
 export const routeGetListingReviews: RequestHandler = async (req, res, next) => {
   const rawListingID = req.params.listingId;
 
@@ -40,4 +49,20 @@ export const routeGetFacilityReviews: RequestHandler = async (req, res, next) =>
   res.status(200).json({
     data: reviews,
   });
+};
+
+export const routeUpdateReview: RequestHandler = async (req, res, next) => { 
+  const params = UpdateReviewBodySchema.parse(req.body);
+
+  const updatedReview = await updateReview(params);
+
+  res.status(200).json({ data: updatedReview });
+};
+
+export const routeDeleteReview: RequestHandler = async (req, res, next) => { 
+  const reviewId = ObjectIdSchema.parse(req.params.reviewId);
+
+  await deleteReview(reviewId);
+
+  res.status(200).json({ message: 'Review deleted successfully.' });
 };
