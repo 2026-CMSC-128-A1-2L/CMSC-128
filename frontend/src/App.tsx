b7c1e1f454c5f0a5f598f7439b06e2e57ed0ddd7
Haira-Espinocilla <hdespinocilla@up.edu.ph>
2026-04-14 11:47:16 +0800

    Added rate and review page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ContactUs from "./pages/ContactUs";
 import PageLayout from "./pages/PageLayout";
 import TestPage from "./pages/TestPage";
+import RateAndReview from "./pages/RateAndReview";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<ContactUs />} />
           <Route path="/test" element={<TestPage />} />
+          <Route path="/ratereview" element={<RateAndReview />} />
         </Routes>
       </PageLayout>
     </Router>
