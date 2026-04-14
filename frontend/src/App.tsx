45de1ae4ad31545ac57a32e8cd6f397ff93d272c
AJBDECASTRO <abdecastro4@up.edu.ph>
2026-04-14 11:48:29 +0800

    feat: Settings (Perferences) Page - Complete Icons, Missing SIdebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ContactUs from "./pages/ContactUs";
 import PageLayout from "./pages/PageLayout";
 import TestPage from "./pages/TestPage";
+import SettingsPreferences from "./pages/SettingsPreferences";
 
 function App() {
 
@@ -11,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<ContactUs />} />
           <Route path="/test" element={<TestPage />} />
+          <Route path="/settings/preferences" element={<SettingsPreferences />} />
+
         </Routes>
       </PageLayout>
     </Router>
