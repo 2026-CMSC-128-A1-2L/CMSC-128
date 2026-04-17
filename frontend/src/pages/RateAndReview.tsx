1b39681ba380090fa6de63e84fd0a40f11c85bc7
Haira-Espinocilla <hdespinocilla@up.edu.ph>
2026-04-17 21:31:15 +0800

    feat: added buttons

--- frontend/src/pages/RateAndReview.tsx
+++ frontend/src/pages/RateAndReview.tsx
@@ -121,86 +121,98 @@ const RateAndReview: FunctionComponent = () => {
                           													<div className="self-stretch flex flex-col items-start gap-[17px] text-num-14">
                             														<div className="self-stretch flex items-center gap-[21px]">
                               															<div className="h-[45.3px] w-[346.7px] relative">
-                                																<div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] bg-whitesmoke-200" />
-                                																
-                                																<b className="absolute h-[59.6%] w-[95.04%] top-[19%] left-[4.97%] flex items-center">What will happen next?</b>
-                                  																	</div>
-                                  																	<div className="h-[45.3px] w-[346.7px] relative">
-                                    																		<div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] bg-whitesmoke-200" />
-                                    																		
-                                    																		<b className="absolute h-[59.6%] w-[95.04%] top-[19%] left-[4.97%] flex items-center">What will happen next?</b>
-                                      																			</div>
-                                      																			</div>
-                                      																			<div className="self-stretch flex items-center gap-[21px]">
-                                        																				<div className="h-[45.3px] w-[346.7px] relative">
-                                          																					<div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] bg-whitesmoke-200" />
-                                          																					
-                                          																					<b className="absolute h-[59.6%] w-[95.04%] top-[19%] left-[4.97%] flex items-center">What will happen next?</b>
-                                            																						</div>
-                                            																						<div className="h-[45.3px] w-[346.7px] relative">
-                                              																							<div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] bg-whitesmoke-200" />
-                                              																						
-                                              																							<b className="absolute h-[59.6%] w-[95.04%] top-[19%] left-[4.97%] flex items-center">What will happen next?</b>
-                                                																								</div>
-                                                																								</div>
-                                                																								</div>
-                                                																								</div>
-                                                																								</div>
-                                                																								</div>
-                                                																								<div className="self-stretch flex items-center justify-center gap-4 text-center text-num-14 text-teal font-inter">
-                                                  																									<div className="h-8 rounded-2xl flex items-center justify-center py-num-0 px-4 box-border cursor-pointer" onClick={onUserProfileTextClick}>
-                                                    																										<b className="relative text-transparent !bg-clip-text [background:linear-gradient(180deg,_#c00f0f,_#e44f4f)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">Go Back</b>
-                                                  																									</div>
-                                                  																									<div className="h-8 rounded-2xl bg-aliceblue flex items-center justify-center py-num-0 px-4 box-border cursor-pointer" onClick={onUserProfileTextClick}>
-                                                    																										<b className="relative">Proceed</b>
-                                                  																									</div>
-                                                																								</div>
-                                                																								</div>
-                                                																								</div>
-                                                																								</div>
-                                                																								{/* <div className="self-stretch h-20 bg-white overflow-hidden shrink-0 flex flex-col items-center justify-center text-center text-dimgray font-inter">
-                                                  																									<div className="w-[1273px] bg-whitesmoke-100 overflow-hidden flex items-center py-[19px] pl-[200px] pr-20 box-border shrink-0">
-                                                    																										<div className="flex-1 flex items-center gap-20">
-                                                      																											<div className="flex items-center gap-4">
-                                                        																												<div className="flex items-center gap-2">
-                                                          																													<img className="w-12 relative max-h-full object-cover" alt="" />
-                                                          																													<div className="flex items-center gap-3">
-                                                            																														<div className="flex items-center gap-1">
-                                                              																															<img className="h-5 w-5 relative" alt="" />
-                                                              																															<b className="relative">2026</b>
-                                                            																														</div>
-                                                            																														<div className="flex items-center justify-center">
-                                                              																															<b className="relative">ATLAS Team</b>
-                                                            																														</div>
-                                                          																													</div>
-                                                        																												</div>
-                                                        																												<div className="flex items-center justify-center">
-                                                          																													<b className="relative">{`All Rights Reserved `}</b>
-                                                        																												</div>
-                                                      																											</div>
-                                                      																											<div className="flex items-center justify-center">
-                                                        																												<div className="flex flex-col items-center justify-center gap-2.5">
-                                                          																													<b className="relative">Browse Dorms</b>
-                                                          																													<b className="relative">List your property</b>
-                                                        																												</div>
-                                                      																											</div>
-                                                      																											<div className="flex flex-col items-center justify-center gap-2.5">
-                                                        																												<b className="relative">About</b>
-                                                        																												<b className="relative">Contact Us</b>
-                                                      																											</div>
-                                                      																											<div className="flex flex-col items-center justify-center gap-2.5">
-                                                        																												<b className="relative">Privacy Policy</b>
-                                                        																												<b className="relative">Terms of Use</b>
-                                                      																											</div>
-                                                    																										</div>
-                                                  																									</div>
-                                                																								</div> */}
-                                                																								</div>
-                                                																								</div>
-                                                																								</div>
-                                                																								</div>
-                                                																								</div>);
-                                              																							};
+                                																<button className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] p-0 m-0 border-none bg-transparent cursor-pointer group">
+																									<div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] bg-whitesmoke-200 transition-colors group-hover:bg-[#e0e0e0]" />
+																									<b className="absolute h-[59.6%] w-[95.04%] top-[19%] left-[4.97%] flex items-center text-left text-black font-lora">
+																										What will happen next?
+																									</b>
+																								</button>
+                                  																</div>
+                                  														<div className="h-[45.3px] w-[346.7px] relative">
+                                    															<button className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] p-0 m-0 border-none bg-transparent cursor-pointer group">
+																									<div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] bg-whitesmoke-200 transition-colors group-hover:bg-[#e0e0e0]" />
+																									<b className="absolute h-[59.6%] w-[95.04%] top-[19%] left-[4.97%] flex items-center text-left text-black font-lora">
+																										What will happen next?
+																									</b>
+																								</button>
+                                      													</div>
+                                      												</div>
+                                      												<div className="self-stretch flex items-center gap-[21px]">
+                                        												<div className="h-[45.3px] w-[346.7px] relative">
+                                          														<button className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] p-0 m-0 border-none bg-transparent cursor-pointer group">
+																									<div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] bg-whitesmoke-200 transition-colors group-hover:bg-[#e0e0e0]" />
+																									<b className="absolute h-[59.6%] w-[95.04%] top-[19%] left-[4.97%] flex items-center text-left text-black font-lora">
+																										What will happen next?
+																									</b>
+																								</button>
+                                            											</div>
+                                            											<div className="h-[45.3px] w-[346.7px] relative">
+                                              													<button className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] p-0 m-0 border-none bg-transparent cursor-pointer group">
+																									<div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] bg-whitesmoke-200 transition-colors group-hover:bg-[#e0e0e0]" />
+																									<b className="absolute h-[59.6%] w-[95.04%] top-[19%] left-[4.97%] flex items-center text-left text-black font-lora">
+																										What will happen next?
+																									</b>
+																								</button>
+                                                										</div>
+                                                									</div>
+                                                							</div>
+                                                						</div>
+                                                				</div>
+                                                			</div>
+															<div className="self-stretch flex items-center justify-center gap-4 text-center text-num-14 text-teal font-inter">
+																<div className="h-8 rounded-2xl flex items-center justify-center py-num-0 px-4 box-border cursor-pointer" onClick={onUserProfileTextClick}>
+																		<b className="relative text-transparent !bg-clip-text [background:linear-gradient(180deg,_#c00f0f,_#e44f4f)] [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]">Go Back</b>
+																</div>
+																<div className="h-8 rounded-2xl bg-aliceblue flex items-center justify-center py-num-0 px-4 box-border cursor-pointer" onClick={onUserProfileTextClick}>
+																		<b className="relative">Proceed</b>
+																</div>
+															</div>
+													</div>
+												</div>
+											</div>
+											{/* <div className="self-stretch h-20 bg-white overflow-hidden shrink-0 flex flex-col items-center justify-center text-center text-dimgray font-inter">
+												<div className="w-[1273px] bg-whitesmoke-100 overflow-hidden flex items-center py-[19px] pl-[200px] pr-20 box-border shrink-0">
+														<div className="flex-1 flex items-center gap-20">
+															<div className="flex items-center gap-4">
+																	<div className="flex items-center gap-2">
+																		<img className="w-12 relative max-h-full object-cover" alt="" />
+																		<div className="flex items-center gap-3">
+																				<div className="flex items-center gap-1">
+																					<img className="h-5 w-5 relative" alt="" />
+																					<b className="relative">2026</b>
+																				</div>
+																				<div className="flex items-center justify-center">
+																					<b className="relative">ATLAS Team</b>
+																				</div>
+																		</div>
+																	</div>
+																	<div className="flex items-center justify-center">
+																		<b className="relative">{`All Rights Reserved `}</b>
+																	</div>
+															</div>
+															<div className="flex items-center justify-center">
+																	<div className="flex flex-col items-center justify-center gap-2.5">
+																		<b className="relative">Browse Dorms</b>
+																		<b className="relative">List your property</b>
+																	</div>
+															</div>
+															<div className="flex flex-col items-center justify-center gap-2.5">
+																	<b className="relative">About</b>
+																	<b className="relative">Contact Us</b>
+															</div>
+															<div className="flex flex-col items-center justify-center gap-2.5">
+																	<b className="relative">Privacy Policy</b>
+																	<b className="relative">Terms of Use</b>
+															</div>
+														</div>
+												</div>
+											</div> */}
+									</div>
+							</div>
+						</div>
+				</div>
+			</div>);
+};
                                               																							
-                                              																							export default RateAndReview ;
+export default RateAndReview ;
                                               																							
