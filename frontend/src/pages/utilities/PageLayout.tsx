106af9507dcd6eea913d2e76572502ad64105994
Daphne Canape <dcanape@up.edu.ph>
2026-04-18 08:00:27 +0800

    feat: added notification_settings screen, initial screens for DM page and fixed background layout as a component

--- frontend/src/pages/utilities/PageLayout.tsx
+++ frontend/src/pages/utilities/PageLayout.tsx
@@ -1,9 +1,9 @@
-import { Outlet } from "react-router-dom";
-import PageBackground from "../../components/PageBackground";
+import { Outlet } from 'react-router-dom';
+import PageBackground from '../../components/PageBackground';
 
 const PageLayout = () => {
   return (
-    <div className="relative min-h-screen w-full max-w-[1440px] mx-auto overflow-x-hidden">
+    <div className="relative">
       <PageBackground />
       <div className="relative z-10">
         <Outlet />
