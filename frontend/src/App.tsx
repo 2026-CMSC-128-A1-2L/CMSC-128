0cea2cdcbc6ab2f36bb0c996b4dbb929187548c5
JB Santos <joelbenedict0810@gmail.com>
2026-04-20 05:25:13 +0800

    feat: initial Finance page for user end, complete icons including popups, no sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,11 +7,15 @@ import UserLanding from './pages/user/UserLanding';
 import SettingsNotifications from './pages/user/SettingsNotifications';
 import LandingMap from './components/LandingMap';
 import HomePage from './pages/user/HomePage';
+import Finance from './pages/user/Finance';
 
 import Login from './pages/utilities/Login';
 import DormCard from './components/DormCard';
 import Upload from './pages/utilities/Upload';
 import DmsLanding from './pages/user/DmsLanding';
+import SubmitReceipt from './components/SubmitReceipt';
+import PaymentMethods from './components/PaymentMethods';
+import FinancePopup from './components/FinancePopup';
 
 function App() {
   return (
@@ -20,6 +24,9 @@ function App() {
         <Route path="/landing" element={<UserLanding />} />
         <Route path="/map" element={<LandingMap />} />
         <Route path="/dormcard" element={<DormCard />} />
+        <Route path="/submit-receipt" element={<SubmitReceipt />} />
+        <Route path="/paymentmethods" element={<PaymentMethods />} />
+        <Route path="/financepopup" element={<FinancePopup children={undefined} />} />
 
         <Route element={<PageLayout />}>
           <Route path="/home" element={<HomePage />} />
@@ -30,6 +37,7 @@ function App() {
           <Route path="/bookmark" element={<BookmarksNewUsers />} />
           <Route path="/settings-notifications" element={<SettingsNotifications />} />
           <Route path="/dms-landing" element={<DmsLanding />} />
+          <Route path="/finance" element={<Finance />} />
         </Route>
       </Routes>
     </Router>
