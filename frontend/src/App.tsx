d64de97e711966ee364a88b6877196920588f935
Quevin Custodio <qacustodio@up.edu.ph>
2026-04-20 00:32:27 +0800

    feat: made the property tabs component work, and interact with one another in the unitdetails page with working transition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import TestPage from "./pages/utilities/TestPage";
 import BookmarksNewUsers from "./pages/user/BookmarksNewUsers";
 import UserLanding from "./pages/user/UserLanding";
 import LandingMap from "./components/LandingMap";
+import UnitDetails from "./pages/user/UnitDetails";
+import PropertyTabs from "./components/unitdetails/PropertyTabs";
 
 import Login from "./pages/utilities/Login";
 import Upload from "./pages/utilities/Upload";
@@ -13,15 +15,18 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/landing" element={<UserLanding />} />
+        <Route path="/" element={<UserLanding />} />
         <Route path="/map" element={<LandingMap />} />
+        <Route path="/test-tab" element={<PropertyTabs />} />
 
         <Route element={<PageLayout />}>
-          <Route path="/" element={<ContactUs />} />
+
+          <Route path="/contact" element={<ContactUs />} />
           <Route path="/login" element={<Login />} />
           <Route path="/upload" element={<Upload />} />
           <Route path="/test" element={<TestPage />} />
           <Route path="/bookmark" element={<BookmarksNewUsers />} />
+          <Route path="/unit" element={<UnitDetails />} />
 
         </Route>
       </Routes>
