83e2c70fe83fdcf74d35d9f81eacf09412b2b5ff
AJDC1021 <abdecastro4@up.edu.ph>
2026-04-20 05:38:56 +0800

    feat: added sidebar and footer, made it more responsive

--- frontend/src/pages/SettingsPreferences.tsx
+++ frontend/src/pages/SettingsPreferences.tsx
@@ -4,6 +4,7 @@ import checked_radio_button from '../../assets/checked_button.svg';
 import unchecked_radio_button from '../../assets/unchecked_button.svg';
 
 import SideBar from '../components/SideBar';
+import Footer from '../components/Footer';
 
 
 
@@ -14,36 +15,36 @@ const SettingsPreferences: FunctionComponent = () => {
   	}, []);
   	
   	return (
-    		<div className="w-full h-[1024px] relative overflow-y-auto flex flex-col items-start isolate gap-2.5 text-left text-num-14 text-darkslategray-100 font-lora">
-      			<img className="w-[1440px] h-[1024px] absolute !!m-[0 important] top-[0px] left-[0px] shrink-0 z-[0]" alt="" />
-      			<div className="w-[1440px] h-[1192px] overflow-hidden shrink-0 flex flex-col items-start z-[1]">
-        				<div className="self-stretch flex-1 overflow-hidden flex flex-col items-start py-0 pl-0 pr-20">
+    		<div className="w-full min-h-screen relative overflow-y-auto flex flex-col items-start isolate gap-2.5 text-left text-num-14 text-darkslategray-100 font-lora">
+      			<img className="w-full h-full absolute !!m-[0 important] top-[0px] left-[0px] shrink-0 z-[0]" alt="" />
+      			<div className="w-full overflow-hidden shrink-0 flex flex-col items-start z-[1]">
+        				<div className="self-stretch flex-1 overflow-hidden flex flex-col items-start py-0 pl-0 pr-4 sm:pr-20">
           					<div className="self-stretch flex-1 flex items-center">
-            						<div className="self-stretch w-[200px] flex items-start shrink-0" />
-            						<div className="h-[1112px] hidden flex-col items-center shrink-0">
-              							<div className="w-[106px] h-[924px] bg-white border-whitesmoke-200 border-solid border-[1px] box-border overflow-hidden shrink-0 flex flex-col items-center py-num-32 pl-num-32 pr-num-10" />
-            						</div>
-            						<div className="self-stretch w-[1240px] overflow-hidden shrink-0 flex flex-col items-start justify-between gap-0">
-              							<div className="self-stretch flex-1 flex flex-col items-start pt-16 pb-0 pl-num-32 pr-20">
-                								<div className="self-stretch h-[1012px] flex flex-col items-start gap-3">
-                  									<div className="w-[1128px] h-16 overflow-hidden shrink-0 hidden items-center p-num-10 box-border gap-2.5">
-                    										<div className="h-6 w-[89px] hidden items-center gap-1.5">
+            						<SideBar />
+            					<div className="hidden flex-col items-center shrink-0">
+              						<div className="bg-white border-whitesmoke-200 border-solid border-[1px] box-border overflow-hidden shrink-0 flex flex-col items-center py-num-32 pl-num-32 pr-num-10" />
+            					</div>
+            					<div className="self-stretch flex-1 overflow-hidden shrink-0 flex flex-col items-start justify-between gap-0">
+            						<div className="self-stretch flex-1 flex flex-col items-start pt-16 pb-0 pl-6 sm:pl-num-32 pr-4 sm:pr-20">
+                					<div className="self-stretch flex-1 flex flex-col items-start gap-3">
+                  						<div className="w-full h-16 overflow-hidden shrink-0 hidden items-center p-num-10 box-border gap-2.5">
+                    							<div className="h-6 w-auto hidden items-center gap-1.5">
                       											<div className="relative font-medium hidden shrink-0">View Tenants</div>
                       											<img className="h-6 w-6 relative hidden shrink-0" alt="" />
                       											<div className="relative font-medium hidden shrink-0">All</div>
                     										</div>
-                    										<div className="w-[704px] rounded-xl bg-aliceblue overflow-hidden shrink-0 flex items-center py-num-10 px-6 box-border gap-2.5 text-dimgray font-inter">
+                    								<div className="flex-1 max-w-3xl rounded-xl bg-aliceblue overflow-hidden shrink-0 flex items-center py-num-10 px-6 box-border gap-2.5 text-dimgray font-inter">
                       											<img className="h-6 w-6 relative" alt="" />
                       											<b className="relative">Search for Dorms, Apartments, or Locations (e.g. UPLB, Umali Subdivision)</b>
                     										</div>
                   									</div>
-                  									<div className="self-stretch h-[948px] flex flex-col items-start gap-12 text-[24px] text-black font-inter">
+                  									<div className="self-stretch flex-1 flex flex-col items-start gap-12 text-[24px] text-black font-inter">
                     										<div className="self-stretch flex items-center">
                       											<b className="relative leading-8">Settings</b>
                     										</div>
                     										<div className="self-stretch flex-1 overflow-hidden flex flex-col items-start text-num-14 text-dimgray">
-                      											<div className="self-stretch rounded-t-num-16 rounded-b-num-0 border-whitesmoke-200 border-solid border-[1px] overflow-hidden flex items-center py-0 px-12 gap-12">
-                        												<div className="h-12 flex flex-col items-center justify-center pt-4 px-0 pb-0 box-border cursor-pointer" onClick={onGeneralContainerClick}>
+                      											<div className="self-stretch rounded-t-num-16 rounded-b-num-0 border-whitesmoke-200 border-solid border-[1px] overflow-hidden flex items-center py-0 px-6 sm:px-12 gap-6 sm:gap-12 flex-wrap sm:flex-nowrap">
+                        												<div className="h-12 flex flex-col items-center justify-center pt-4 px-0 pb-0 box-border cursor-pointer text-xs sm:text-sm" onClick={onGeneralContainerClick}>
                           													<div className="flex items-center justify-center">
                             														<b className="relative">General</b>
                           													</div>
@@ -70,7 +71,7 @@ const SettingsPreferences: FunctionComponent = () => {
                           													</div>
                         												</div>
                       											</div>
-                      											<div className="self-stretch rounded-t-num-0 rounded-b-num-16 border-whitesmoke-200 border-solid border-[1px] overflow-hidden flex flex-col items-start py-6 px-num-32 gap-6 text-center text-[24px] text-black">
+                      											<div className="self-stretch rounded-t-num-0 rounded-b-num-16 border-whitesmoke-200 border-solid border-[1px] overflow-hidden flex flex-col items-start py-6 px-6 sm:px-num-32 gap-6 text-center text-[24px] text-black">
                         												<div className="self-stretch flex flex-col items-start">
                           													<b className="relative leading-8">Display</b>
                         												</div>
@@ -81,8 +82,8 @@ const SettingsPreferences: FunctionComponent = () => {
                               															<div className="self-stretch relative leading-6 font-medium text-darkslategray-100 text-left">Choose how ATLAS looks to you. Selections are applied immediately and saved automatically.</div>
                             														</div>
                           													</div>
-                          													<div className="w-[916px] overflow-hidden flex items-center justify-center py-num-10 px-0 box-border gap-9">
-                            														<div className="w-[438px] flex flex-col items-start gap-2.5">
+                          						<div className="w-full overflow-hidden flex flex-col lg:flex-row items-center justify-center py-num-10 px-0 box-border gap-9">
+                            							<div className="w-full lg:w-1/2 flex flex-col items-start gap-2.5">
                               															<div className="self-stretch h-[220px] rounded-num-16 bg-white border-whitesmoke-200 border-solid border-[1px] box-border overflow-hidden shrink-0 flex flex-col items-start gap-2">
                                 																<div className="self-stretch h-10 rounded-t-num-16 rounded-b-num-0 bg-darkslategray-200 overflow-hidden shrink-0 flex items-start p-num-10 box-border" />
                                 																<div className="self-stretch flex flex-col items-start">
@@ -120,7 +121,7 @@ const SettingsPreferences: FunctionComponent = () => {
                                 																</div>
                               															</div>
                             														</div>
-                            														<div className="w-[438px] flex flex-col items-start gap-2.5">
+                            														<div className="w-full lg:w-1/2 flex flex-col items-start gap-2.5">
                               															<div className="self-stretch h-[220px] rounded-num-16 bg-black border-black border-solid border-[1px] box-border overflow-hidden shrink-0 flex flex-col items-start gap-2">
                                 																<div className="self-stretch h-10 rounded-t-num-16 rounded-b-num-0 bg-darkslategray-200 overflow-hidden shrink-0 flex items-start p-num-10 box-border" />
                                 																<div className="self-stretch flex flex-col items-start">
@@ -165,43 +166,7 @@ const SettingsPreferences: FunctionComponent = () => {
                   									</div>
                 								</div>
               							</div>
-              							<div className="self-stretch h-20 bg-white overflow-hidden shrink-0 flex flex-col items-center justify-center text-center text-dimgray font-inter">
-                								<div className="w-[1273px] bg-whitesmoke-100 overflow-hidden flex items-center py-[19px] pl-[200px] pr-20 box-border shrink-0">
-                  									<div className="flex-1 flex items-center gap-20">
-                    										<div className="flex items-center gap-4">
-                      											<div className="flex items-center gap-2">
-                        												<img className="w-12 relative max-h-full object-cover" alt="" />
-                        												<div className="flex items-center gap-3">
-                          													<div className="flex items-center gap-1">
-                            														<img className="h-5 w-5 relative" alt="" />
-                            														<b className="relative">2026</b>
-                          													</div>
-                          													<div className="flex items-center justify-center">
-                            														<b className="relative">ATLAS Team</b>
-                          													</div>
-                        												</div>
-                      											</div>
-                      											<div className="flex items-center justify-center">
-                        												<b className="relative">{`All Rights Reserved `}</b>
-                      											</div>
-                    										</div>
-                    										<div className="flex items-center justify-center">
-                      											<div className="flex flex-col items-center justify-center gap-2.5">
-                        												<b className="relative">Browse Dorms</b>
-                        												<b className="relative">List your property</b>
-                      											</div>
-                    										</div>
-                    										<div className="flex flex-col items-center justify-center gap-2.5">
-                      											<b className="relative">About</b>
-                      											<b className="relative">Contact Us</b>
-                    										</div>
-                    										<div className="flex flex-col items-center justify-center gap-2.5">
-                      											<b className="relative">Privacy Policy</b>
-                      											<b className="relative">Terms of Use</b>
-                    										</div>
-                  									</div>
-                								</div>
-              							</div>
+              							<Footer />
             						</div>
           					</div>
         				</div>
