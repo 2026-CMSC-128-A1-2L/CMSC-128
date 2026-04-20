4dbf19aef5a6ce73cf90ff2bea5d171a7867b2f2
kbdoroja <kbdoroja@up.edu.ph>
2026-04-21 05:03:33 +0800

    Feat: added Finalized Application Page (real) , with footer and sidebar implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,11 +15,14 @@ import RateAndReviewForm from './pages/RateAndReviewWithForms';
 import RateAndReviewUpload from './pages/RateAndReviewWithUploads';
 import TermsOfUse from './pages/TermsOfUse';
 import Finance from './pages/user/Finance';
+import FinalizedApplication1b from './pages/user/FInalizedApplicationPage1b';
 
 import Login from './pages/utilities/Login';
 import SideBar from './components/SideBar';
 import Upload from './pages/utilities/Upload';
 import DmsLanding from './pages/user/DmsLanding';
+import DmsSidebar from './components/DmsSidebar';
+import DormCard from "./components/DormCard";
         
 import SettingsPreferences from './pages/user/SettingsPreferences';
 import SubmitReceipt from './components/SubmitReceipt';
@@ -65,6 +68,7 @@ function App() {
           <Route path="/ratereviewupload" element={<RateAndReviewUpload />} />
           <Route path="/terms-of-use" element={<TermsOfUse />} />
           <Route path="/finance" element={<Finance />} />
+          <Route path="/finalizedapplication1b" element={<FinalizedApplication1b/>} />
           
           {/* Admin Routes */}
           <Route path="/admin/analytics" element={<Analytics />} />
