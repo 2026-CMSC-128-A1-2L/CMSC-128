a303f0f270cd5f62cb35e081fc907204a939ba53
Quevin Custodio <qacustodio@up.edu.ph>
2026-04-21 23:14:00 +0800

    feat: added sidebar and adjusted the size of the outer divs to fit the screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ContactUs from "./pages/ContactUs";
-import PageLayout from "./pages/PageLayout";
 import TestPage from "./pages/TestPage";
 import SettingsSecurity from "./pages/SettingsSecurity";
 
@@ -8,13 +7,11 @@ function App() {
 
   return (
     <Router>
-      <PageLayout>
-        <Routes>
-          <Route path="/" element={<ContactUs />} />
-          <Route path="/test" element={<TestPage />} />
-          <Route path="/settingsSecurity" element={<SettingsSecurity />} />
-        </Routes>
-      </PageLayout>
+      <Routes>
+        <Route path="/" element={<ContactUs />} />
+        <Route path="/test" element={<TestPage />} />
+        <Route path="/settingsSecurity" element={<SettingsSecurity />} />
+      </Routes>
     </Router>
 
   );
