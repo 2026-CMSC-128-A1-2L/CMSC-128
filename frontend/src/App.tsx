a80829c1f025913f1f65c96b925380d8011af579
Quevin Custodio <qacustodio@up.edu.ph>
2026-04-21 23:00:23 +0800

    fix: fixed font style, added a component footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ContactUs from "./pages/ContactUs";
-import PageLayout from "./pages/PageLayout";
 import TestPage from "./pages/TestPage";
 import SettingsGen from "./pages/SettingsGen";
 
@@ -8,13 +7,11 @@ function App() {
 
   return (
     <Router>
-      <PageLayout>
-        <Routes>
-          <Route path="/" element={<ContactUs />} />
-          <Route path="/test" element={<TestPage />} />
-          <Route path="/settings" element={<SettingsGen />}/>
-        </Routes>
-      </PageLayout>
+      <Routes>
+        <Route path="/" element={<ContactUs />} />
+        <Route path="/test" element={<TestPage />} />
+        <Route path="/settings" element={<SettingsGen />} />
+      </Routes>
     </Router>
 
   );
