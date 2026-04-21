1380759212de6b66d844649d7efd2bf33bb4d6bf
lvdejesus <lvdejesus1@up.edu.ph>
2026-04-21 12:23:53 +0800

    fix: notification schema

--- shared/src/schemas/common.ts
+++ shared/src/schemas/common.ts
@@ -31,10 +31,10 @@ export const DateTimeSchema = z.iso.datetime().transform((x) => new Date(x));
 
 export const PaginationRequestSchema = (maxLimit: number) =>
   z.object({
-    cursor: z.string(),
+    cursor: z.string().optional(),
     limit: z.int().min(0).max(maxLimit),
   });
 
 export const PaginationResponseSchema = z.object({
-  cursor: z.string(),
+  cursor: z.string().optional(),
 });
