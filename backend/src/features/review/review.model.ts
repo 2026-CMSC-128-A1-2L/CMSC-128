c7db84836540b9fefa28acba7339e5d58946eb0e
Nathaniel Cunanan <cunanannathaniel04@gmail.com>
2026-04-23 15:33:45 +0800

    fix(model): add facilityId sa field, and added rating categories (yung tatlo).

--- backend/src/features/review/review.model.ts
+++ backend/src/features/review/review.model.ts
@@ -4,9 +4,18 @@ const reviewSchema = new mongoose.Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
     listingId: { type: mongoose.Schema.Types.ObjectId, ref: 'Listing', required: true },
-    rating: { type: Number, required: true, min: 1, max: 5 },
+    facilityId: { type: mongoose.Schema.Types.ObjectId, ref: 'HousingFacility', required: true },
+
+    ratings: {
+      quality: { type: Number, required: true, min: 1, max: 5 },
+      comfort: { type: Number, required: true, min: 1, max: 5 },
+      environment: { type: Number, required: true, min: 1, max: 5 }
+    },
+
     description: { type: String },
+
   },
+  
   { timestamps: true },
 );
 
