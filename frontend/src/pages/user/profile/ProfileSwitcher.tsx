8911b053760a07c42b3dccdbea34a114dee904f0
Daphne Canape <dcanape@up.edu.ph>
2026-04-24 03:37:13 +0800

    chore: move portalpopup to components/gen

--- frontend/src/pages/user/profile/ProfileSwitcher.tsx
+++ frontend/src/pages/user/profile/ProfileSwitcher.tsx
@@ -1,10 +1,10 @@
 // pages/user/ProfileSwitcher.tsx
 import { useState, useCallback } from "react";
 import CurrentDorm from "./current_dorm/CurrentDorm";
-import UserVerif from "./verification/UserVerif";
-import Sidebar from "../../../components/user/SideBar";
-import Footer from "../../../components/general/Footer";
-import Switch from "../../../components/user/CurrentDormToVerificationSwitch";
+import UserVerif from "./UserVerif";
+import Sidebar from "../../../components/SideBar";
+import Footer from "../../../components/Footer";
+import Switch from "../../../components/CurrentDormToVerificationSwitch";
 import ProfileInfo from "../../../components/ProfileInfo";
 import { Icon } from "@iconify/react";
 
