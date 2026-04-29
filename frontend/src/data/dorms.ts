651e830feafd7357ae89735f621072871ec7c14e
Daphne Canape <dcanape@up.edu.ph>
2026-04-30 06:50:14 +0800

    feat: changed dorm cards to match landlord properties

--- frontend/src/data/dorms.ts
+++ frontend/src/data/dorms.ts
@@ -1,37 +1,36 @@
-// sample data
-import sapphirePic from '../../assets/sapphire.jpg';
-import sapphirePic1 from '../../assets/sapphire2.jpg';
+import sapphire from "../../assets/sapphire.jpg";
+import sapphire2 from "../../assets/sapphire1.jpg";
 
 export const dormData = [
   {
     id: 1,
-    name: 'Two Sapphire Place',
-    rating: '4.1',
+    name: "One Sapphire Place",
+    rating: "4.1",
     price: {
       min: 7500,
       max: 8000,
     },
-    location: 'Sapphire St., Los Baños',
-    image: sapphirePic,
+    location: "Sapphire St., Los Baños",
+    image: sapphire,
     room_types: [
-      { pax: '1 pax', price: 12000 },
-      { pax: '4 pax', price: 3500 },
+      { pax: "1 pax", price: 12000 },
+      { pax: "4 pax", price: 3500 },
     ],
   },
   {
     id: 2,
-    name: 'One Sapphire Place',
-    rating: '3.7',
+    name: "Emerald Heights",
+    rating: "3.7",
     price: {
       min: 4000,
       max: 8000,
     },
-    location: 'Umali Subdivision, Los Baños',
-    image: sapphirePic1,
+    location: "Umali Subdivision, Los Baños",
+    image: sapphire2,
     room_types: [
-      { pax: '1 pax', price: 15000 },
-      { pax: '4 pax', price: 4000 },
-      { pax: '4 pax', price: 3500 },
+      { pax: "1 pax", price: 15000 },
+      { pax: "4 pax", price: 4000 },
+      { pax: "4 pax", price: 3500 },
     ],
   },
 ];
