ac913c5bb6aec322c47ed4bab21219ef4acac5c1
Daphne Canape <dcanape@up.edu.ph>
2026-05-02 16:23:33 +0800

    fix: layout of frames (phase 2 and 3)

--- frontend/src/components/user/messages/InvitationButtons.tsx
+++ frontend/src/components/user/messages/InvitationButtons.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent } from "react";
 
 interface InvitationButtonsProps {
   onAccept: () => void;
@@ -12,7 +12,8 @@ const InvitationButtons: FunctionComponent<InvitationButtonsProps> = ({
   return (
     <div className="flex items-center text-left text-[20.87px] text-crimson">
       <div className="flex items-center gap-[23.9px]">
-        <div className="w-[148px] rounded-[17.89px] flex items-center justify-center py-[11.9px] px-[35.8px] box-border cursor-pointer hover:opacity-80 transition-opacity"
+        <div
+          className="w-[148px] rounded-[17.89px] flex items-center justify-center py-[11.9px] px-[35.8px] box-border cursor-pointer hover:opacity-80 transition-opacity"
           onClick={onCancel}
         >
           <div className="relative font-semibold inline-block max-w-[401.25px]">
